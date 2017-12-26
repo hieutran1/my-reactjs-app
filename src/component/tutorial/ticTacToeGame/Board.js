2c0a63ad24bb19272b66119275659a046019f507
Hieu Tran <hieutran@kms-technology.com>
2017-12-26 11:48:19 +0700

    inplementing time travel

--- src/component/tutorial/ticTacToeGame/Board.js
+++ src/component/tutorial/ticTacToeGame/Board.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import Square from './Square';
-import calculate from '../../../logic/calculate';
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  
   renderSquare(i) {
     return (
     <Square 
@@ -22,17 +12,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner) {
-      status = 'winner: ' + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? 'X': 'O');
-    }
-    
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -54,23 +35,3 @@ class Board extends React.Component {
 }
 export default Board
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for(let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }    
-  }
-  return null;
-}
